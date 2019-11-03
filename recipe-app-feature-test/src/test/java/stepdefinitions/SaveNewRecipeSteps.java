0ad3f71f30e1f1ee1b26eae7b27dee8b9d9d3b8e
Subhadip Dey <subhadip.dey@telenetgroup.be>
2019-11-03 20:26:22 +0100

    #9 set up BDD testing in the project

---
+++ recipe-app-feature-test/src/test/java/stepdefinitions/SaveNewRecipeSteps.java
@@ -0,0 +1,27 @@
+package stepdefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class SaveNewRecipeSteps {
+
+    @Given("Alice has logged into the application")
+    public void alice_has_logged_into_the_application() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("Alice submits a new recipe with a name")
+    public void alice_submits_a_new_recipe_with_a_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("Recipe is saved")
+    public void recipe_is_saved() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+}
---
+++ recipe-app-feature-test/src/test/resources/features/recipe/saveNewRecipe.feature
@@ -0,0 +1,6 @@
+Feature: Author can save a new recipe in the application
+
+  Scenario: Author can save a new recipe when recipe has a name
+    Given Alice has logged into the application
+    When Alice submits a new recipe with a name
+    Then Recipe is saved
