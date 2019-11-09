515c4c7e536c272fcad13fceab85b0536f6391e6
Subhadip Dey <subhadip.dey@telenetgroup.be>
2019-11-10 00:28:37 +0100

    #10 add skeleton mock recipe persistor adapter

---
+++ recipe-app-feature-test/src/test/java/adapters/recipe/out/MockRecipePersistor.java
@@ -0,0 +1,12 @@
+package adapters.recipe.out;
+
+import com.recipeapp.core.model.Recipe;
+import com.recipeapp.core.port.out.StoreRecipe;
+
+public class MockRecipePersistor implements StoreRecipe {
+
+    @Override
+    public void accept(Recipe recipe) {
+
+    }
+}
